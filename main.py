71042748ee4889f5face8e61d560ab713403ba93
TiredSpirit <tiredspirit12@gmail.com>
2025-05-10 15:32:41 -0700

    Git said the file was modified although it hasnt changed

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +19,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
